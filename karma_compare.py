e00e238060c790530dee37b8685d14800a734e29
Corey Lee <slicklogic@gmail.com>
2014-06-29 12:40:30 -0400

    Version 1.0

--- karma_compare.py
+++ karma_compare.py
@@ -8,45 +8,66 @@ import json
 #while loop switch
 x = True
 
+#check if user name exsists
+def userCheck(user):
+    r = urllib.request.Request('http://www.reddit.com/user/%s.json' % (user))
+    try:
+        urllib.request.urlopen(r)
+    except urllib.error.HTTPError:
+        newUser = input('Does not exsist. Try again. \n')
+        userCheck(newUser)
+        return(newUser)
+    else:
+        return(user)
+
 #store user ID's
 def userInput():
     global id1, id2
 
     id1 = input('Please provide the first user you wish to compare. \n')
+    id1 = userCheck(id1)
     id2 = input('Please provide the second user. \n')
-    
+    id2 = userCheck(id2)
     userInfo(id1, id2)    
 
-#gather users karma information and store it for comparison
+#gather users karma, up votes, and downvote information and stores it for comparison
 def userInfo(user1, user2):
-    global total
+    global total, uTotal, dTotal
 
     userList = [user1, user2]
     total = []
-    
-    for num in userList:
-        #pulls users data from reddit.com/user/
-        for user in userList:
-            score = []
-            rawdata = urllib.request.urlopen('http://www.reddit.com/user/%s.json' % (user)).read()
-            data = json.loads(rawdata.decode('utf8'))
-            results = data['data']['children']
-            #scrapes out relevant json data
-            for eachResult in results:
-                score.append(eachResult['data']['score'])
-                #print(score)
-        print(sum(score)) 
+    uTotal = []
+    dTotal =  []
+    #pulls users data from reddit.com/user/
+    for user in userList:
+        
+        ups = []
+        downs = []
+        score = []
+        results = None  
+        rawdata = urllib.request.urlopen('http://www.reddit.com/user/%s.json' % (user)).read()
+        data = json.loads(rawdata.decode('utf8'))
+        results = data['data']['children']
+        #scrapes out relevant json data
+        
+        for eachResult in results:
+            score.append(eachResult['data']['score'])
+            ups.append(eachResult['data']['ups'])
+            downs.append(eachResult['data']['downs'])
+        
         total.append(sum(score))
-    
-    print(total)
+        uTotal.append(sum(ups))
+        dTotal.append(sum(downs))
+
     compUsers()
 
 #displays differences in users karma score.
 def compUsers():
-    print(total)
-    print(id1,' has a karma score of ', total[0])
+    
+    print(id1,' has a karma score of ', total[0], ' ||up votes:',uTotal[0], '||down votes:',dTotal[0])
     print('-'*10)
-    print(id2,' has a karma score of ', total[1])
+    
+    print(id2,' has a karma score of ', total[1], ' ||up votes:',uTotal[1], '||down votes:',dTotal[1])
     print('-'*10)
 
     if total[0] > total[1]:
@@ -66,4 +87,4 @@ while x == True:
         x = True
     else:
         x = False
- 
+
