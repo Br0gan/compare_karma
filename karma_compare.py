8e661ea943135844ce0b492cee0b25debabe8391
Corey Lee <slicklogic@gmail.com>
2014-06-29 09:56:31 -0400

    almost done

--- karma_compare.py
+++ karma_compare.py
@@ -5,37 +5,66 @@ import urllib.request
 import json
 
 
-id1 = input('Please provide the first user you wish to compare. \n')
-id2 = input('Please provide the second user. \n')
+#while loop switch
+x = True
 
-#userdata1 = None
-#userdata2 = None
+#store user ID's
+def userInput():
+    global id1, id2
 
+    id1 = input('Please provide the first user you wish to compare. \n')
+    id2 = input('Please provide the second user. \n')
+    
+    userInfo(id1, id2)    
+
+#gather users karma information and store it for comparison
 def userInfo(user1, user2):
-    userList = [user1, user2]
-    score = []  
-    total = []
-    
-    for user in userList:
-        rawdata = urllib.request.urlopen('http://www.reddit.com/user/%s.json' % (user)).read()
-        data = json.loads(rawdata.decode('utf8'))
-        results = data['data']['children']
-        
-        for eachResult in results:
-            score.append(eachResult['data']['score'])
-            #print(score)
-    
-    print(len(score))
-    
-    for num in score:
-        if len(total) < 1:
-            x = num
-            print("check")
-        else:
-            x = x + num
-            print("check2")
-    
-    total.append(x)
-    print('This is x', x)
+    global total
 
-userInfo(id1, id2)
+    userList = [user1, user2]
+    total = []
+    score = []
+    
+    for num in userList:
+        #pulls users data from reddit.com/user/
+        for user in userList:
+            score = []
+            rawdata = urllib.request.urlopen('http://www.reddit.com/user/%s.json' % (user)).read()
+            data = json.loads(rawdata.decode('utf8'))
+            results = data['data']['children']
+            #scrapes out relevant json data
+            for eachResult in results:
+                score.append(eachResult['data']['score'])
+                #print(score)
+        print(sum(score)) 
+        total.append(sum(score))
+    
+    print(total)
+    compUsers()
+
+#displays differences in users karma score.
+def compUsers():
+    print(total)
+    print(id1,' has a karma score of ', total[0])
+    print('-'*10)
+    print(id2,' has a karma score of ', total[1])
+    print('-'*10)
+
+    if total[0] > total[1]:
+        diference = total[0] - total[1]
+        print(id1,' wins with ', diference, ' more karma points!')
+    elif total[0] == total[1]:
+        print('It\'s a tie!')
+    else:
+        diference = total[1] - total[0]
+        print(id2,' wins with ', diference, ' more karma points!')
+
+#loop to keep progam alive for more then one cycle
+while x == True:
+    userInput()
+    choice = input('Would you like to compare another 2 users?(y/n) \n')
+    if choice == 'y':
+        x = True
+    else:
+        x = False
+ 
